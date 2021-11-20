8767c39caff435100342708c2b83818ac072ded4
João <74837451+slocknad@users.noreply.github.com>
2021-11-20 20:14:06 +0000

    Let Amanda manage merch ticket channels

--- bot.py
+++ bot.py
@@ -620,7 +620,7 @@ async def create_ticket_channel(init_message,name,user):
     mod_role = user.guild.get_role(int(config['MOD_ROLE_ID']))
     overwrites = {user.guild.default_role: discord.PermissionOverwrite(read_messages=False), user: discord.PermissionOverwrite(read_messages=True, send_messages=False), mod_role:discord.PermissionOverwrite(read_messages=True)}
     if name == "merch-ticket":
-        overwrites[merch_support_role] = discord.PermissionOverwrite(read_messages=True)
+        overwrites[merch_support_role] = discord.PermissionOverwrite(read_messages=True, manage_channel=True)
 
     open_ticket_cat = user.guild.get_channel(int(config['OPEN_TICKET_CAT_ID']))
     channel = await user.guild.create_text_channel(name + "-" + user.name, category= open_ticket_cat, overwrites= overwrites)
