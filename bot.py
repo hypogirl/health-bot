35c63111d6c6aec7b770525614f9fc757d510cac
joão <74837451+slocknad@users.noreply.github.com>
2021-12-26 16:26:24 +0000

    improving musik recognition

--- bot.py
+++ bot.py
@@ -533,7 +533,7 @@ async def on_message(message):
             if message.content and not(message.attachments) and not(aux.check_url(message.content)):
                 await message.delete()
 
-    if message.content.lower() in ["musik make love to " + bot.user.mention,"musik make love to health bot","musik make love to health :: bot"]:
+    if message.content.lower() in ["musik make love to health bot"] or message.guild.get_member(bot.user.id) in message.mentions:
         await asyncio.sleep(1)
         o = ["Oh no... not me.","Why would anyone want this","What is wrong with you?","No no no no no no"]
         await message.channel.send(random.choice(o))
