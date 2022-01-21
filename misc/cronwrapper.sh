e3f3c4a2a167f4c18eafd7972e18fe580bbde30b
Justin Beattie <jbeattie@akamai.com>
2022-01-21 15:31:33 -0600

    updating paths to be more generic

--- misc/cronwrapper.sh
+++ misc/cronwrapper.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-[[ -d $HOME/logs/ ]] || mkdir ~/logs
 
 if ! pgrep -f 'python.*bot.py'
 then
-    echo "$(date) Not running... Starting up" >> /home/hackermans/logs/health-bot.log
-    /usr/bin/python3 -u /home/hackermans/src/health-bot/bot.py >> /home/hackermans/logs/health-bot.log 2>&1 &
-#else
-    # Commenting this out, no need for it anymore
-    #echo "$(date) running" >> /home/hackermans/logs/health-bot.log
+    echo "$(date) Not running... Starting up" >> /var/log/botden/health-bot.log
+    /usr/bin/python3 -u ${HOME}/src/health-bot/bot.py >> /var/log/botden/health-bot.log 2>&1 &
 fi
